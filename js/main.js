7a06b44caca969bc32c65b1f7970c81a0c3eedd7
Anaïs Albersario <anais@MacBook-Air-de-Anais.local>
2025-10-23 14:58:04 +0200

    Section 2 et début Section 3

--- js/main.js
+++ js/main.js
@@ -1,54 +1,53 @@
 gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin);
 
+// Anim déplacement au lancement 
 
+gsap.from("#anim", {
+  
 
-gsap.to("#window2", {
-  ScrollTrigger:{
-    trigger:"section2",
-    start:"top 60%",
-    end:"top 20%",
-    scrub: 1,
-    markers: true
-  },
-  backgroundColor:"rgb(0, 0, 0)",
-  duration: 2,
-  ease: "none"
-});
-
-gsap.to("#window2", {
-  ScrollTrigger:{
-    trigger:"section2",
-    start:"top 60%",
-    end:"top 20%",
-    scrub: 1,
-    markers: true
-  },
-  y: -100, // déplacement vers le haut
-  duration: 3,
+  y: 100, // déplacement vers le haut
+  duration: 5,
   ease: "power2.out"
 });
 
 
 
-// Timeline --------------------------------------------
 
-
-
-let tl = gsap.timeline();
-tl.to("#list1", {duration: 1, x: -200})
-  .to("#list2", {duration: 2, x: -200})
-  .to("#list3", {duration: 2, x: -200})
-
-
+// Timeline + scroll trigger section 2--------------------------------------------
 
 let tl = gsap.timeline({
-  ScrollTrigger:{
-    trigger:"#section2",
-    markers: true,
+  scrollTrigger:{
+    trigger:"#anim",
+    //markers: true,
     start:"top 60%",
     end:"top 20%",
-    toggleActions:"play none reverse set",
+    id:"#texteHaut",
+    toggleActions:"play none reverse reset",
     scrub: 1,
-}
-});
+  }
   
+});
+  tl.from("#list1", {duration: 1, x: -200})
+  .from("#list2", {duration: 2, x: -200})
+  .from("#list3", {duration: 2, x: -200})
+
+  
+
+  tl.from("#list4", {duration: 1, x: 200})
+  .from("#list5", {duration: 2, x: 200})
+  .from("#list6", {duration: 2, x: 200})
+
+
+// Caroussel  section 3--------------------------------------------
+
+let tlCarousel = gsap.timeline({
+  scrollTrigger:{
+    trigger:"#section3",
+    start:"top 80%",
+    end:"bottom 20%",
+    markers: true,
+    toggleActions:'play'
+  },
+  repeat: -1
+
+})
