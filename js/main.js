a1359de7a61438191b00cacc357147f38601bef4
Anaïs Albersario <anais@MacBook-Air-de-Anais.local>
2025-11-01 16:01:37 +0100

    Responsive & Match Media

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 gsap.registerPlugin(ScrollTrigger, Observer, ScrollToPlugin, Draggable, MotionPathPlugin);
 
+//-------------------------------------------------------------------------------
+// ---------------------------Responsive MatchMedia -----------------------------
+//-------------------------------------------------------------------------------
+
+
+let mm = gsap.matchMedia();
+
+mm.add(
+  {
+    isMobile: "(max-width: 799px)",
+    isDesktop: "(min-width: 800px)"
+  },
+  (context) => {
+    let { isMobile, isDesktop } = context.conditions;
+
+
 // Anim déplacement au lancement  -------------------------------------
 
 gsap.from("#anim", {
   
 
-  y: 100, // déplacement vers le haut
-  duration: 5,
+  y: isMobile ? 50 : 100, // déplacement vers le haut
+  duration: isMobile ? 3 : 5,
   ease: "power2.out"
 });
-
 
 
 
@@ -27,15 +42,16 @@ let tl = gsap.timeline({
   }
   
 });
-  tl.from("#list1", {duration: 1, x: -200})  //https://gsap.com/docs/v3/GSAP/Timeline/
-  .from("#list2", {duration: 2, x: -200})
-  .from("#list3", {duration: 2, x: -200})
+
+  tl.from("#list1", {duration: 1, x: isMobile ? -100 : -200 })  //https://gsap.com/docs/v3/GSAP/Timeline/
+  .from("#list2", {duration: 2, x: isMobile ? -100 : -200 })
+  .from("#list3", {duration: 2, x: isMobile ? -100 : -200 })
 
   
 
-  tl.from("#list4", {duration: 1, x: 200})
-  .from("#list5", {duration: 2, x: 200})
-  .from("#list6", {duration: 2, x: 200})
+  tl.from("#list4", {duration: 1, x: isMobile ? 100 : 200})
+  .from("#list5", {duration: 2, x: isMobile ? 100 : 200})
+  .from("#list6", {duration: 2, x: isMobile ? 100 : 200});
 
 
 // Caroussel  section 3--------------------------------------------
@@ -49,9 +65,9 @@ let tlCarousel = gsap.timeline({
 });
 
 tlCarousel.to(".photo", {
-  xPercent: 250,
-  duration: 15,
-})
+  xPercent: isMobile ? 100 : 250,
+  duration: isMobile ? 8 : 15,
+});
 
 // Draggable section 4 -----------------------------------------------
 
@@ -59,3 +75,6 @@ Draggable.create(".tshirt", { // Pour faire essayer les vetements au mannequin
   bounds: "#section4",
   inertia: true
 });
+
+  }
+);
